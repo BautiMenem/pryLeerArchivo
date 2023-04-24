178ba7267b64db38be90b6099a36e8d5d15a5438
menem <menem@DESKTOP-8V5FLBK>
2023-04-24 18:30:25 -0300

    Agregar archivos de proyecto.

---
+++ pryLeerArchivo/frmMain.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace pryLeerArchivo
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.txtGrabar = new System.Windows.Forms.TextBox();
+            this.rtbTexto = new System.Windows.Forms.RichTextBox();
+            this.txtDato = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(271, 103);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(75, 23);
+            this.btnLeer.TabIndex = 0;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(271, 374);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 1;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Location = new System.Drawing.Point(271, 51);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(75, 23);
+            this.btnGrabar.TabIndex = 2;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.btnGrabar_Click);
+            // 
+            // txtGrabar
+            // 
+            this.txtGrabar.Location = new System.Drawing.Point(36, 51);
+            this.txtGrabar.Name = "txtGrabar";
+            this.txtGrabar.Size = new System.Drawing.Size(190, 20);
+            this.txtGrabar.TabIndex = 3;
+            // 
+            // rtbTexto
+            // 
+            this.rtbTexto.Location = new System.Drawing.Point(36, 103);
+            this.rtbTexto.Name = "rtbTexto";
+            this.rtbTexto.ReadOnly = true;
+            this.rtbTexto.Size = new System.Drawing.Size(190, 168);
+            this.rtbTexto.TabIndex = 4;
+            this.rtbTexto.Text = "";
+            // 
+            // txtDato
+            // 
+            this.txtDato.Location = new System.Drawing.Point(36, 311);
+            this.txtDato.Name = "txtDato";
+            this.txtDato.Size = new System.Drawing.Size(190, 20);
+            this.txtDato.TabIndex = 5;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(271, 308);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 6;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(377, 409);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtDato);
+            this.Controls.Add(this.rtbTexto);
+            this.Controls.Add(this.txtGrabar);
+            this.Controls.Add(this.btnGrabar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLeer);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Leer Archivo";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnGrabar;
+        private System.Windows.Forms.TextBox txtGrabar;
+        private System.Windows.Forms.RichTextBox rtbTexto;
+        private System.Windows.Forms.TextBox txtDato;
+        private System.Windows.Forms.Button btnBuscar;
+    }
+}
+
---
+++ pryLeerArchivo/frmMain.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryLeerArchivo
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        { 
+            StreamWriter ManejoArchivo = new StreamWriter("ArchivoNuevo.txt", true);
+            string Texto = txtGrabar.Text;
+            using (StreamWriter sw = ManejoArchivo)
+            {
+                sw.WriteLine(Texto);
+            }
+            MessageBox.Show("Archivo guardado correctamente");
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            //Nuevo Archivo
+            //StreamWriter ManejoArchivo = new StreamWriter("ArchivoNuevo.txt", true);
+
+            //Usar archivo ya creado
+            //StreamWriter LeerArchivo = new StreamWriter("Libro.txt");
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            StreamReader ManejoArchivo = new StreamReader("ArchivoNuevo.txt");
+            using (StreamReader sr = new StreamReader("ArchivoNuevo.txt"))
+            {
+                string Contenido = sr.ReadToEnd();
+                rtbTexto.Text = Contenido;
+            }
+            MessageBox.Show("Leido Correctamente");
+
+            while (ManejoArchivo.EndOfStream == false)
+            {
+            
+              string Auxiliar = ManejoArchivo.ReadLine();
+
+              if (Auxiliar.Contains(txtDato.Text))
+              {
+                MessageBox.Show("Encontrado");
+                break;
+              }
+              else
+              {
+                    MessageBox.Show("No encontrado");
+              }
+
+            } 
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            //StreamReader ManejoArchivo = new StreamReader("ArchivoNuevo.txt");
+
+            //while (ManejoArchivo.EndOfStream == false)
+            //{
+            //    string Auxiliar = ManejoArchivo.ReadLine();
+
+            //    if (Auxiliar.Contains(txtDato.Text))
+            //    {
+            //        MessageBox.Show("Encontrado");
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("No encontrado");
+            //    }
+
+            //}
+        }
+    }
+}
